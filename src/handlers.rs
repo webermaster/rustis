817e376e1bfb18c28ac0f6b37c01f40eeda5c5aa
Webermaster <cpu.kmw@gmail.com>
2025-08-09 18:36:59 -0500

    added dynamic echo

--- src/handlers.rs
+++ src/handlers.rs
@@ -10,8 +10,12 @@ pub fn init_handler_funcs() -> HashMap<&'static str, HandlerFunc> {
     m
 }
 
-pub fn ping(_: Vec<Message>) -> Message {
-    Message::String("PONG".to_string())
+pub fn ping(args: Vec<Message>) -> Message {
+    match args.as_slice() {
+        [] => Message::String("PONG".to_string()),
+        [Message::Bulk(arg), _rest @ ..] => Message::String(arg.to_string()),
+        _ => Message::Error("Protocol error: expected Bulk string".to_string())
+    }
 }
 
 #[cfg(test)]
@@ -33,6 +37,13 @@ mod tests {
         let handler = handlers.get("PING").unwrap();
         let result = handler(vec![]);
         assert_eq!(result, Message::String("PONG".to_string()));
+    }
+
+    #[test]
+    fn test_ping_handler_with_args() {
+        let pong = "foo".to_string();
+        let result = ping(vec![Message::Bulk(pong.clone())]);
+        assert_eq!(result, Message::String(pong));
     }
 }
 
